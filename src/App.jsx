d4ffd489ce552c5ea9c86c7c53a6688da80e737d
Sonam <sonamlodhi6251@gmail.com>
2024-01-09 23:27:43 +0530

    implement props and add css

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,15 @@ import Card from './components/Card/Card'
 function App() {
  
   return (
-    <>
+    <div className='card-wrapper'>
+      <Card title="Dog 1" src="https://images.pexels.com/photos/160846/french-bulldog-summer-smile-joy-160846.jpeg?auto=compress&cs=tinysrgb&w=600"/>
+      <Card title="Dog 2" src="https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg?auto=compress&cs=tinysrgb&w=600"/>
       <Card />
-    </>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
   )
 }
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,12 @@
-const Card = () => {
+import './Card.css'
+
+const Card = ({title="Dog", src="https://images.pexels.com/photos/1490908/pexels-photo-1490908.jpeg?auto=compress&cs=tinysrgb&w=600"}) => {
 
     return(
         <div className="card">
-            my card
+            <img src={src} alt="dog card" />
+            <h1>{title}</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus necessitatibus similique eum, porro odio quisquam nihil quasi culpa quam ipsum aperiam nulla, dignissimos voluptas fuga asperiores facilis in perspiciatis omnis?</p>
         </div>
     )
 }
